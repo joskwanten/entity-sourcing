8d207dc58412f3d0d2c6cf8f6b08425c7a718137
Jos <jos.kwanten@gmail.com>
2017-10-07 13:35:11 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+﻿# Entity sourcing
+
+An experiment to make a universal REST backend based on event sourcing where events are stored in a simple text-file and played back as soon as the application restarts.
+You can create rest resouces on the fly by simpely posting an entity to the supposed resouce.
+
+E.g if you want to create a cars resource, then you can post the following message
+```bash
+> curl -H "Content-Type: application/json" -X POST -d '{"brand":"BMW","type":"320D"}' http://localhost:3000/api/v1/cars
+```
+
+Once an entity is created, it can be (partially updated) with a Post command
+```bash
+> curl -H "Content-Type: application/json" -X POST -d '{"brand":"BMW","type":"320D", "power":"139kW"}' http://localhost:3000/api/v1/cars/bc06f74c-dfff-46b1-8c82-00b3f4de71db
+```
+
+To delete an entity, it can be done by using the DELETE verb:
+```bash
+> curl -H "Content-Type: application/json" -X DELETE http://localhost:3000/api/v1/cars/bc06f74c-dfff-46b1-8c82-00b3f4de71db
+```
+
+## Retrieving
+Entitys can be retrieved by a GET operation on the entity url:
+```bash
+> curl http://localhost:3000/api/v1/cars
+```
+Or
+```bash
+> curl http://localhost:3000/api/v1/cars/bc06f74c-dfff-46b1-8c82-00b3f4de71db
+```
+Query parameters are also supported. E.g.
+```bash
+> curl http://localhost:3000/api/v1/cars?brand=BMW
+```
---
+++ app.js
@@ -0,0 +1,199 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const fs = require('fs');
+const readline = require('readline');
+const uuidv4 = require('uuid/v4');
+
+const journal = 'data/events.txt';
+const port = 3000;
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+// Static content
+app.use(express.static('public'));
+
+// Add CORS header
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+// Router for /api
+// get an instance of the express Router
+let router = express.Router();
+
+let aggregates = {};
+
+// Read the events from the file
+var rd = readline.createInterface({
+    input: fs.createReadStream(journal),
+    output: process.stdout,
+    console: false
+});
+
+// Replay events at startup to get the balances of the accounts
+rd.on('line', function(line) {
+    processEvent(JSON.parse(line));
+});
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
+});
+
+function getProperies(object, callback) {
+    for (var property in object) {
+        if (object.hasOwnProperty(property)) {
+            callback(property);
+        }
+    }
+}
+
+router.get('/v1/:entity', function(req, res) {
+    if (aggregates[req.params.entity] != null) {
+        // return the objects that match with the supplied query parameters if there are any
+        res.json(aggregates[req.params.entity].filter(e => {
+            let match = true;
+            getProperies(req.query, (property) => {
+                if (req.query[property] != e[property]) {
+                    match = false;
+                }
+            });
+
+            return match;
+        }));
+        return;
+    }
+
+    // Not found, send 404
+    res.sendStatus(404);
+});
+
+router.get('/v1/:entity/:id', function(req, res) {
+    if (aggregates[req.params.entity] != null) {
+        let result = aggregates[req.params.entity].find(a => a.id === req.params.id)
+        if (result !== undefined) {
+            res.json(result);
+            return;
+        }
+    }
+
+    // Not found, send 404
+    res.sendStatus(404);
+});
+
+router.post('/v1/:entity', function(req, res) {
+    console.log(`Received Entity: '${req.params.entity}' JSON: '${req.body}'`);
+
+    processCommand({
+        entity: req.params.entity,
+        event: 'create',
+        payload: req.body
+    });
+
+    res.sendStatus(202);
+});
+
+
+router.put('/v1/:entity/:id', function(req, res) {
+    console.log(`Received Entity: '${req.params.entity}' JSON: '${req.body}'`);
+
+    if (aggregates[req.params.entity] != null &&
+        aggregates[req.params.entity].find(a => a.id === req.params.id) !== undefined) {
+
+        processCommand({
+            entity: req.params.entity,
+            event: 'update',
+            payload: Object.assign(req.body, { id: req.params.id })
+        });
+
+        res.sendStatus(202);
+    } else {
+        // Not found, send 404
+        res.sendStatus(404);
+    }
+});
+
+router.delete('/v1/:entity/:id', function(req, res) {
+    console.log(`Received Entity: '${req.params.entity}' Delete`);
+
+    if (aggregates[req.params.entity] != null &&
+        aggregates[req.params.entity].find(a => a.id === req.params.id) !== undefined) {
+
+        processCommand({
+            entity: req.params.entity,
+            event: 'delete',
+            id: req.params.id
+        });
+
+        res.sendStatus(202);
+    } else {
+        // Not found, send 404
+        res.sendStatus(404);
+    }
+});
+
+// more routes for our API will happen here
+
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// Start the server
+app.listen(port);
+
+console.log('Application is running on ' + port);
+
+function processCommand(command) {
+    let event = command;
+    event.timestamp = new Date();
+
+    event.id = uuidv4();
+
+    // Add an entity id in case of a create
+    if (event.event === 'create') {
+        event.payload.id = uuidv4();
+    }
+
+    // Store event
+    // These two methods should be refactored to a pub/sub mechanism for larger applications
+    fs.appendFileSync(journal, JSON.stringify(event) + "\n");
+
+    // Update the account information
+    processEvent(event);
+}
+
+function processEvent(event) {
+    switch (event.event) {
+        case 'create':
+            if (!aggregates[event.entity]) {
+                aggregates[event.entity] = []
+            }
+            aggregates[event.entity].push(event.payload);
+            break;
+        case 'update':
+            if (aggregates[event.entity]) {
+                let itemIndex = aggregates[event.entity].findIndex(e => e.id === event.payload.id);
+                if (itemIndex > -1) {
+                    aggregates[event.entity][itemIndex] =
+                        aggregates[event.entity][itemIndex] = event.payload;
+                }
+            }
+            break;
+        case 'delete':
+            if (aggregates[event.entity]) {
+                let itemIndex = aggregates[event.entity].findIndex(e => e.id === event.payload.id);
+                if (itemIndex > -1) {
+                    aggregates[event.entity].splice(itemIndex, 1);
+                }
+            }
+            break;
+    }
+}
